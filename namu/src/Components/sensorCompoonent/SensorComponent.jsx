ec7f9d167198e57f349945e992e1fbfb36530575
kirannjr11 <ghalekiran03@gmail.com>
2026-02-02 13:32:04 +0900

    created sensor page

--- namu/src/Components/sensorCompoonent/SensorComponent.jsx
+++ namu/src/Components/sensorCompoonent/SensorComponent.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import "./SensorComponent.css"
 
+import temp from "../../assets/namu-temp-dark.svg"
+import humi from "../../assets/namu-humi-dark.svg"
+import ec from "../../assets/namu-ec-dark.svg"
+import ph from "../../assets/namu-ph-dark.svg"
+
 
 function SensorComponent() {
   return (
@@ -10,6 +15,59 @@ function SensorComponent() {
         </div>
 
         <div className="sensorc-inner-div">
+            <div className="sensorc-inner-type1">
+                <div className="sensorc-inner-image">
+                    <img src = {temp} alt = "temp image" />
+                </div>
+                <div className="sensorc-inner-details">
+                    <div className="sensorc-inner-title">온도</div>
+                    <div className="sensorc-value-units">
+                        <div className="sensorc-inner-sensor-value">12</div>
+                        <div className="sensorc-inner-sensor-unit">°C</div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="sensorc-inner-type1">
+                <div className="sensorc-inner-image">
+                    <img src = {humi} alt = "temp image" />
+                </div>
+                <div className="sensorc-inner-details">
+                    <div className="sensorc-inner-title">습도</div>
+                    <div className="sensorc-value-units">
+                        <div className="sensorc-inner-sensor-value">12</div>
+                        <div className="sensorc-inner-sensor-unit">%</div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="sensorc-inner-type1">
+                <div className="sensorc-inner-image">
+                    <img src = {ec} alt = "temp image" />
+                </div>
+                <div className="sensorc-inner-details">
+                    <div className="sensorc-inner-title">EC</div>
+                    <div className="sensorc-value-units">
+                        <div className="sensorc-inner-sensor-value">12</div>
+                        <div className="sensorc-inner-sensor-unit">ds/m</div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="sensorc-inner-type1">
+                <div className="sensorc-inner-image">
+                    <img src = {ph} alt = "temp image" />
+                </div>
+                <div className="sensorc-inner-details">
+                    <div className="sensorc-inner-title">PH</div>
+                    <div className="sensorc-value-units">
+                        <div className="sensorc-inner-sensor-value">12</div>
+                        <div className="sensorc-inner-sensor-unit">μmol/m²/s </div>
+                    </div>
+                </div>
+            </div>
+
             <div className="sensorc-inner-type1">
                 <div className="sensorc-inner-image">
                     <img src = "src/assets/namu-temp-dark.svg" alt = "temp image" />
