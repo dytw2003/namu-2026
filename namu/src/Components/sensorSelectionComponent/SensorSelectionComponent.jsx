d38321ac807dac028b3143d2725cf55f6c60353f
kirannjr11 <ghalekiran03@gmail.com>
2026-02-03 22:08:17 +0900

    applies toast

--- namu/src/Components/sensorSelectionComponent/SensorSelectionComponent.jsx
+++ namu/src/Components/sensorSelectionComponent/SensorSelectionComponent.jsx
@@ -2,54 +2,115 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
-
-import { useTheme } from "../../providers/ThemeProvider/ThemeProvider"
-
+import { useTheme } from "../../providers/ThemeProvider/ThemeProvider";
 import "./SensorSelectionComponent.css";
-function SensorSelectionComponent() {
 
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function SensorSelectionComponent() {
   const { theme } = useTheme();
+
+  const showNotReady = () => {
+    toast.info("서비스 준비 중입니다.", {
+      position: "bottom-center",
+      autoClose: 2000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: false,
+    });
+  };
+
   return (
-    <div className= {`graph-house-checkbox-maindiv ${theme}`}>
+    <div className={`graph-house-checkbox-maindiv ${theme}`}>
       <div className="graph-title-close">
-        <div className="graph-title">Graph</div>
+        <div className="graph-title">통계 분석</div>
         <div className="graph-open-close">펼치기</div>
       </div>
+
       <div className="house-wrapper">
-        <div className="sensor-selection">Select sensor</div>
+        <div className="sensor-selection">센서 선택</div>
+
         <div className="graph-house-checkbox-innerdiv">
-          <input type="checkbox" id="house_0" />
-          <label htmlFor="house_0" className="custom-checkbox">
+          <input type="checkbox" id="sensor_temp" />
+          <label htmlFor="sensor_temp" className="custom-checkbox">
             <FontAwesomeIcon icon={faCheck} className="check-icon" />
           </label>
           <span className="checkbox-label-text">온도</span>
         </div>
 
-        {/* 1동 ~ 11동 */}
         <div className="graph-house-checkbox-innerdiv">
-          <input type="checkbox" id="house_1" />
-          <label htmlFor="house_1" className="custom-checkbox">
+          <input type="checkbox" id="sensor_hum" />
+          <label htmlFor="sensor_hum" className="custom-checkbox">
             <FontAwesomeIcon icon={faCheck} className="check-icon" />
           </label>
           <span className="checkbox-label-text">습도</span>
         </div>
 
         <div className="graph-house-checkbox-innerdiv">
-          <input type="checkbox" id="house_2" />
-          <label htmlFor="house_2" className="custom-checkbox">
+          <input type="checkbox" id="sensor_ec" />
+          <label htmlFor="sensor_ec" className="custom-checkbox">
             <FontAwesomeIcon icon={faCheck} className="check-icon" />
           </label>
           <span className="checkbox-label-text">EC</span>
         </div>
 
         <div className="graph-house-checkbox-innerdiv">
-          <input type="checkbox" id="house_3" />
-          <label htmlFor="house_3" className="custom-checkbox">
+          <input type="checkbox" id="sensor_ph" />
+          <label htmlFor="sensor_ph" className="custom-checkbox">
             <FontAwesomeIcon icon={faCheck} className="check-icon" />
           </label>
           <span className="checkbox-label-text">PH</span>
         </div>
       </div>
+
+      <div className="graph-time-container">
+        <div className="time-text-wrapper">
+          <div className="graph-time-text">기간</div>
+        </div>
+
+        <div className="graph-dandt">
+          <div className="graph-date-selection">
+            <div className="graph-starting-date">
+              <DatePicker
+                selected={null}
+                placeholderText="YYYY-MM-DD"
+                dateFormat="yyyy-MM-dd"
+                onChange={() => {}}
+                readOnly
+                onInputClick={showNotReady}
+              />
+            </div>
+
+            <div className="graph-seperation-sign">~</div>
+
+            <div className="graph-starting-date">
+              <DatePicker
+                selected={null}
+                placeholderText="YYYY-MM-DD"
+                dateFormat="yyyy-MM-dd"
+                onChange={() => {}}
+                readOnly
+                onInputClick={showNotReady}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <ToastContainer
+  position="top-center"
+  autoClose={2000}
+  hideProgressBar
+  className="toast-center-container"
+  toastClassName="toast-center"
+  bodyClassName="toast-center-body"
+/>
+
     </div>
   );
 }
